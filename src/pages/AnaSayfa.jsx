77a326dfff025ef1a4bfc4faa025646c8b2fa494
Onur Şahin <boteonur@gmail.com>
2026-04-29 15:37:25 +0300

    oyna metinleri silme

--- src/pages/AnaSayfa.jsx
+++ src/pages/AnaSayfa.jsx
@@ -8,13 +8,11 @@ export default function AnaSayfa() {
         {/* Deme Oyunu Butonu */}
         <Link to="/deme" className="bg-white p-6 rounded-3xl shadow-xl hover:scale-105 transition-transform flex flex-col items-center">
           <img src="/logo.png"/>
-          <h2 className="text-2xl font-bold">Deme! Oyunu</h2>
         </Link>
         
         {/* Locked Checker Butonu */}
         <Link to="/lockedchecker" className="bg-white p-6 rounded-3xl shadow-xl hover:scale-105 transition-transform flex flex-col items-center">
           <div className="w-48 h-32 bg-indigo-50 text-indigo-600 mb-4 rounded-2xl flex items-center justify-center font-black text-xl border-4 border-indigo-100 shadow-inner">
-            LOCKED CHECKER
           </div>
           <h2 className="text-2xl font-bold text-gray-800">Locked Checker</h2>
         </Link>
