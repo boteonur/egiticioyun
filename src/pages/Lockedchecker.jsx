40cbc1d6d78b8e81c01c8d9569e4b8b804e4837c
Onur Şahin <boteonur@gmail.com>
2026-04-29 13:43:23 +0300

    Lockedchecker export hatası çözüldü

---
+++ src/pages/Lockedchecker.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Lockedchecker() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-indigo-50 p-4">
+      <h1 className="text-4xl md:text-5xl font-black text-indigo-600 mb-4 text-center">
+        Locked Checker
+      </h1>
+      <p className="text-xl text-indigo-400 font-bold">
+        Bu oyun çok yakında burada olacak!
+      </p>
+    </div>
+  );
+}
