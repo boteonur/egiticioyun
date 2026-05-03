7ec8c4bc576880cd297d4a850c05b777b03d886b
Onur <boteonur@gmail.com>
2026-05-03 22:26:21 +0300

    logo düzenleme

--- src/pages/AnaSayfa.jsx
+++ src/pages/AnaSayfa.jsx
@@ -4,16 +4,31 @@ import { Link } from 'react-router-dom';
 export default function AnaSayfa() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-8">
-      <div className="flex gap-8">
+      {/* Container: Mobilde alt alta (flex-col), bilgisayarda yan yana (md:flex-row) */}
+      <div className="flex flex-col md:flex-row gap-8 w-full max-w-4xl justify-center items-center">
         
         {/* Deme Oyunu Butonu ve Logosu */}
-        <Link to="/deme" className="bg-white p-8 rounded-3xl shadow-xl hover:scale-105 text-center flex flex-col items-center justify-center">
-          <img src="/deme_logo.png" alt="Deme Oyunu" className="w-48 h-32 object-contain" />
+        <Link 
+          to="/deme" 
+          className="bg-white p-8 md:p-12 rounded-[2.5rem] shadow-xl hover:scale-105 transition-transform flex flex-col items-center justify-center w-full md:w-1/2"
+        >
+          <img 
+            src="/deme_logo.png" 
+            alt="Deme Oyunu" 
+            className="w-full h-40 md:h-48 object-contain drop-shadow-md" 
+          />
         </Link>
         
         {/* Locked Checker Butonu ve Logosu */}
-        <Link to="/lockedchecker" className="bg-white p-8 rounded-3xl shadow-xl hover:scale-105 text-center flex flex-col items-center justify-center">
-          <img src="/lc_logo.png" alt="Locked Checker" className="w-48 h-32 object-contain" />
+        <Link 
+          to="/lockedchecker" 
+          className="bg-white p-8 md:p-12 rounded-[2.5rem] shadow-xl hover:scale-105 transition-transform flex flex-col items-center justify-center w-full md:w-1/2"
+        >
+          <img 
+            src="/lc_logo.png" 
+            alt="Locked Checker" 
+            className="w-full h-40 md:h-48 object-contain drop-shadow-md" 
+          />
         </Link>
         
       </div>
