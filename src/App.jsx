39ceca0c2d1259b7e25e62e2ef3323a626a84516
Onur <boteonur@gmail.com>
2026-05-03 20:42:47 +0300

    oyun ayarları yapıldı

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+git add .import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AnaSayfa from './pages/AnaSayfa';
 import Deme from './pages/Deme';
